d485f7d84fdb79c1a6664b9239b12edc2bdcf69f
eunhyun <kesther4669@naver.com>
2025-10-29 16:29:31 +0900

    1029 commit

--- week09.py
+++ week09.py
@@ -1,12 +1,33 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-titanic = sns.load_dataset('titanic')
-# print(titanic.info())
-# print(titanic.tail(3))
-print(titanic.query('age >= 70'))
+df = sns.load_dataset('penguins')
+# print(df.info())
+#      Column             Non-Null Count  Dtype  
+# ---  ------             --------------  -----  
+#  0   species            344 non-null    object 
+#  1   island             344 non-null    object 
+#  2   bill_length_mm     342 non-null    float64
+#  3   bill_depth_mm      342 non-null    float64
+#  4   flipper_length_mm  342 non-null    float64
+#  5   body_mass_g        342 non-null    float64
+#  6   sex                333 non-null    object     -> 성별 누락된게 11개
 
-sns.scatterplot(x='age', y='fare', data=titanic)   # 산점도그리기
-plt.show()
+# print(df.head())
+# print(df.tail())
+
+# print(df.describe())
+# print(df['body_mass_g'].describe())
+# print(df['bill_depth_mm']>=55.0)  # true, false로 출력됨
+
+
+print(df[df['bill_length_mm']>=55.0])
+#        species  island  bill_length_mm  bill_depth_mm  flipper_length_mm  body_mass_g     sex
+# 169  Chinstrap   Dream            58.0           17.8              181.0       3700.0  Female
+# 215  Chinstrap   Dream            55.8           19.8              207.0       4000.0    Male
+# 253     Gentoo  Biscoe            59.6           17.0              230.0       6050.0    Male
+# 321     Gentoo  Biscoe            55.9           17.0              228.0       5600.0    Male
+# 335     Gentoo  Biscoe            55.1           16.0              230.0       5850.0    Male
+
